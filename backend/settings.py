e922d75eba373aa5471567b003765fc2ebbb049a
CBeatrix <120434975+CBeatrixBacolanta@users.noreply.github.com>
2025-04-09 21:54:37 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://cbeatrixbacolanta.github.io/celeste-todo",
+    "https://cbeatrixbacolanta.github.io",
 ]
 
 MIDDLEWARE = [
